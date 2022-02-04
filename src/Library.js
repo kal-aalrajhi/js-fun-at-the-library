3ed1a9d95a32eeb42fc1aa5dbe24ae108c491939
Kal <kal.aalrajhi@gmail.com>
2022-02-04 16:38:59 -0700

    Complete all Library functions

--- src/Library.js
+++ src/Library.js
@@ -15,41 +15,24 @@ function addBook(library, book) {
   library.shelves[book.genre].push(book); // must use bracket notation to make it dynamic
 }
 
-// function checkoutBook(library, bookTitle, genre) {
-//   // navigate to genre array
-//   console.log(library.shelves[])
-//
-// }
-//
-//
-// var dracula = {
-//   title: "Dracula",
-//   mainCharacter: { name: "Count Dracula", age: undefined, pronouns: "he/him" },
-//   pageCount: 418,
-//   genre: "fantasy"
-// }
-// var bornACrime = {
-//   title: "Born a Crime",
-//   mainCharacter: { name: "Trevor Noah", age: 36, pronouns: "he/him" },
-//   pageCount: 304,
-//   genre: "nonFiction"
-// }
-// var prideAndPrejudice = {
-//   title: "Pride and Prejudice",
-//   mainCharacter: { name: "Eliabeth Bennet", age: 20, pronouns: "she/her" },
-//   pageCount: 432,
-//   genre: "fiction"
-// }
-// var denverLibrary = createLibrary("Denver Public Library");
-//
-// addBook(denverLibrary, dracula);
-// addBook(denverLibrary, bornACrime);
-// addBook(denverLibrary, prideAndPrejudice);
-//
-// var result1 = checkoutBook(denverLibrary, "Pride and Prejudice", "fiction");
+function checkoutBook(library, bookTitle, genre) {
+  // navigate to respective genre shelf and check for book title
+  var shelf = library.shelves[genre] // array of books
+  // iterate over array of book objects to find if book title is in array
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      // remove book from shelf
+      shelf.splice(i,1); // user array method splice to remove only 1 item at the ith index
+      // let user know they have successfully check out the book
+      return `You have now checked out ${bookTitle} from the ${library.name}`;
+    }
+  }
+  // loop did not find book, let user know
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`;
+}
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
