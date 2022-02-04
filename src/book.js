90a8eec1e68ba12fa1f1cdf7ece47aaa58ae09b4
Kal <kal.aalrajhi@gmail.com>
2022-02-03 18:42:30 -0700

    Complete saveReview function

--- src/book.js
+++ src/book.js
@@ -12,10 +12,37 @@ function buildMainCharacter(name, age, pronouns) {
   return character;
 }
 
+function saveReview(review, reviews){
+  // Check for unique reviews
+  var duplicate = false; // assume no duplicate
+  var i = 0;
+
+  // Iterate through array looking for duplicates
+  for (var i = 0; i < reviews.length; i++) {
+    if (review === reviews[i]) duplicate = true;
+  }
+  // Append review to passed array only if there are no duplicates
+  if (!duplicate) reviews.push(review);
+}
+
+// test for saveReview
+var reviews = ["Garbage!", "Good stuff.", "An astounding success", "Nope."];
+saveReview("An astounding success", reviews);
+console.log(reviews);
+
+// while (!duplicate) {
+//   if (review === reviews[i]) {
+//     console.log("DUPLICATE!");
+//     duplicate = true;
+//   }
+//   // Go to next review
+//   i++;
+// }
+
 module.exports = {
   createTitle,
   buildMainCharacter,
-  // saveReview,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
