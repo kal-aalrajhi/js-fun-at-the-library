84418ed97c3b70500ccd8c9fd615daf41b2e9f12
Kal <kal.aalrajhi@gmail.com>
2022-02-06 16:41:49 -0700

    Complete constructor, greenPatron, and findBook methods

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,25 @@
 class Librarian {
-  constructor() {
-    
+  // (string, object)
+  constructor(name, library) {
+    this.name = name;
+    this.library = library; // stores a library object
+  }
+
+  // Greet patron by name based on time of day
+  greetPatron(patronName, isMorning) {
+    if (isMorning) return `Good morning, ${patronName}!`;
+    else return `Hello, ${patronName}!`;
+  }
+
+  // Find book bases off book title
+  findBook(bookTitle) {
+    // Store genre based shelf array
+    var genreShelf = this.library.shelves['fantasy']; // make dynamic
+    // Check if book is on shelf
+    if (genreShelf.includes(bookTitle)) return `Yes we have ${bookTitle}`;
+    else return `Sorry, we do not have ${bookTitle}`;
   }
 }
+
 
 module.exports = Librarian;
