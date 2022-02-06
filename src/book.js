41370e72843bf7e418e4c7e11ec76e50b8215f9a
Kal <kal.aalrajhi@gmail.com>
2022-02-06 09:02:21 -0700

    Tidy up code and add function comments

--- src/book.js
+++ src/book.js
@@ -1,7 +1,9 @@
+// Takes in a string and concatinates "The" before it.
 function createTitle(title) {
   return "The " + title;
 }
 
+// Constructs a character object and returns that object
 function buildMainCharacter(name, age, pronouns) {
   var character = {
     name: name,
@@ -12,21 +14,38 @@ function buildMainCharacter(name, age, pronouns) {
   return character;
 }
 
-function saveReview(review, reviews){
-  // Check for unique reviews
-  var duplicate = false; // assume no duplicate
-  var i = 0;
-
-  // Iterate through array looking for duplicates
-  for (var i = 0; i < reviews.length; i++) {
-    if (review === reviews[i]) duplicate = true;
-  }
-  // Append review to passed array only if there are no duplicates
-  if (!duplicate) reviews.push(review);
+// Add reviews to an array, but won't add duplicate reviews.
+function saveReview(review, reviewsArr) {
+  if (!reviewsArr.includes(review)) reviewsArr.push(review);
 }
 
+/*
+  Alternate way to do saveReview function:
+  -doesn't use .includes method
+*/
+// function saveReview(review, reviewsArr){
+//   // Check if review unique
+//   var duplicate = false; // assume no duplicate
+//   var i = 0;
+//
+//   // Iterate through array looking for duplicates
+//   for (var i = 0; i < reviewsArr.length; i++) {
+//     if (review === reviewsArr[i]) duplicate = true;
+//   }
+//   // Append review to passed array only if there are no duplicates
+//   if (!duplicate) reviewsArr.push(review);
+// }
+
+// counts the number of letters in title and multiplies it by 20
 function calculatePageCount(bookTitle) {
-  // // Alternate way to do it and skip spaces, so we actaully only count letters
+  return bookTitle.length * 20;
+}
+
+/*
+  Alternate way to do calculatePageCount function:
+  - doesn't count spaces as a letter
+*/
+// function calculatePageCount(bookTitle) {
   // var letterCount = bookTitle.length
   // for (var i = 0; i < bookTitle.length; i++) {
   //   Avoid counting spaces as letters
@@ -34,20 +53,21 @@ function calculatePageCount(bookTitle) {
   //   letterCount++;
   // }
   // return letterCount * 20;
-  return bookTitle.length * 20;
-}
+// }
 
+// creates and returns a book object
 function writeBook(title, mainCharacter, genre) {
-  // Create an object using these three parameters
   var book = {
     title: title,
     mainCharacter: mainCharacter,
     genre: genre,
-    pageCount: calculatePageCount(title), // Invoke calculatePageCount function to get number of pages for book
+    // Invoke calculatePageCount function to get number of pages for book
+    pageCount: calculatePageCount(title),
   };
   return book;
 }
 
+// Reduces the number of pages in a book by 1/4.
 function editBook(book) {
   book.pageCount *= (0.75);
 }
