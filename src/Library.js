464776ca6d080570a340bb0a0bcf8845ee07524a
Kal <kal.aalrajhi@gmail.com>
2022-02-06 10:46:53 -0700

    Tidy up code and add function comments

--- src/Library.js
+++ src/Library.js
@@ -1,5 +1,5 @@
+// Creates and populates library object
 function createLibrary(name) {
-  // return library object
   return {
     name: name,
     // Create shelves property which has an object value of shelf parameters containing book arrays
@@ -11,23 +11,27 @@ function createLibrary(name) {
   };
 }
 
+// Add book to library genre specific shelf
 function addBook(library, book) {
-  library.shelves[book.genre].push(book); // must use bracket notation to make it dynamic
+  // Use bracket notation, instead of dot notation, to access shelves dynamically
+  library.shelves[book.genre].push(book);
 }
 
+// Checkout book
 function checkoutBook(library, bookTitle, genre) {
-  // navigate to respective genre shelf and check for book title
-  var shelf = library.shelves[genre] // array of books
-  // iterate over array of book objects to find if book title is in array
+  // Array of book objects on genre specific shelf
+  var shelf = library.shelves[genre]
+  // Iterate over shelf titles to find if book title is available for checkout
   for (var i = 0; i < shelf.length; i++) {
+    // Book is available
     if (shelf[i].title === bookTitle) {
-      // remove book from shelf
-      shelf.splice(i,1); // user array method splice to remove only 1 item at the ith index
-      // let user know they have successfully check out the book
+      // Remove one book from shelf (the current book object (i))
+      shelf.splice(i,1);
+      // Let user know they have successfully check out the book
       return `You have now checked out ${bookTitle} from the ${library.name}`;
     }
   }
-  // loop did not find book, let user know
+  // Loop did not find book, let user know
   return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`;
 }
 
